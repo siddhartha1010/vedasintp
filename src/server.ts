0fb7271f4a0d3380d30d19e4402c55df1efb0a03
siddhartha1010 <siddhartha.raut@codavatar.tech>
2024-07-21 13:48:31 +0545

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, {Application,Request,Response} from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello Worldsssss!aaa')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
